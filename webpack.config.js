64b09464e7414b73b0f209b49824488276e46510
Omar Zikry <omar@omarzikry.com>
2021-01-18 12:59:49 +0200

    feat: add img loader setting to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+          },
+        },
+      },
     ],
   },
   devtool: "eval-cheap-module-source-map",
